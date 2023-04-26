af530cb197abee3b60ee9339e3787252cf0e56a3
Liam Riche <liamr@kainos.com>
2023-04-26 09:44:08 +0100

    update report and add fix for note stitching

--- process.py
+++ process.py
@@ -2,70 +2,69 @@ import math
 import pandas as pd
 
 df = pd.read_csv('./545blow_basic_pitch.csv', usecols=range(4))
-
+print("original dataset")
 print(df.to_string())
 
+#filtering out notes that are too quiet
 df = df[df.velocity >= 55]
 
-
-
-
+#sort results by pitch and by start time
 df = df.sort_values(['pitch_midi', 'end_time_s'],ascending=[True,True])
-
+print("sorted results of dataframe")
 print(df.to_string())
 
-
-
-
-print(math.isclose(0.626938776, 0.638548753, abs_tol = 0.02))
-
-new_df = df[df['start_time_s'].isin(df['end_time_s'].values)]
-
-print(new_df.to_string())
-
-#cols = ['start_time_s', 'end_time_s']
-#index = 3
-#df2 = df[(df[cols]==df.loc[index, cols]).all(1)]
-
-
-#df2 = df[df.duplicated(keep=False)]
-#print(df2)
-
-
-
-
-
-#for x in df.index:
-#    if df[]
-#  print(x)
-
+##create dataframe of notes that are identical and have less than 20ms release and reapply (most likely the same note)
+#temp data converter
 data=df
 
+#creating differential column for dataset
 diffData=[]
 for i in range(1, len(data)):
     diffData.append((data.iloc[i,0]) - (data.iloc[i-1,1]))
 
-#print(diffData)
-
 data['diff']=[0] + diffData
+print
 print(data)
-
-print("testing loc")
 
 
 def getStartEnd(tempData,THRESHOLD):
     tempData=tempData.reset_index()
     finalData=[]
     startTime=tempData.loc[0,'start_time_s']
+    print(tempData.loc[0,'start_time_s'])
+
+    print("tempData:")
+    print(tempData)
+
     for i in range(1,len(tempData)):
-        if tempData.loc[i,'diff'] < THRESHOLD and tempData.loc[i,'diff'] >= 0 and (tempData.loc[i,'pitch_midi'] == tempData.loc[i-1,'pitch_midi']):
-            finalData.append([startTime,tempData.loc[i-1,'end_time_s'], [tempData.loc[i,'pitch_midi']]])
+        if THRESHOLD > tempData.loc[i, 'diff'] >= 0 and (tempData.loc[i, 'pitch_midi'] == tempData.loc[i - 1, 'pitch_midi']):
+
+            print("pitch1 start was ")
+            print(tempData.loc[i, 'start_time_s'])
+            print(tempData.loc[i, 'pitch_midi'])
+            print(" pitch2 end was ")
+            print(tempData.loc[i, 'end_time_s'])
+            print(tempData.loc[i - 1, 'pitch_midi'])
+            if i == 1:
+                finalData.append([startTime, tempData.loc[i,'end_time_s'], tempData.loc[i,'pitch_midi']])
+            else:
+                finalData.append([tempData.loc[i-1,'start_time_s'], tempData.loc[i, 'end_time_s'], tempData.loc[i, 'pitch_midi']])
             startTime=tempData.loc[i,'start_time_s']
-    #finalData.append([startTime,tempData.loc[i,'end_time_s'],[tempData.loc[i,'pitch_midi']]])
-    return(pd.DataFrame([finalData]))
+            print("new start time is")
+            print(startTime)
+    #finalData.append([tempData.loc[i-1, 'start_time_s'], tempData.loc[i, 'end_time_s'], tempData.loc[i,'pitch_midi']])
+    #finalData=finalData.reset_index()
+    return(pd.DataFrame(finalData))
 
-finalData=pd.DataFrame(columns=['start_time_s','end_time_s','pitch_midi'])
-for user in data['pitch_midi']:
-    finalData=pd.concat([finalData,getStartEnd(data,0.02)])
 
+finalData=pd.DataFrame()
+
+print("finalData:")
 print(finalData)
+
+#call function and place results back into itself
+finalData=pd.concat([finalData,getStartEnd(data,0.02)])
+print("final results")
+print(finalData)
+
+#replace split notes from from original
