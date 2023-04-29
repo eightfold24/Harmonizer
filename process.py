85e7a191cf01046d8b5e4bb5aac4e5695374a399
Liam Riche <liamr@kainos.com>
2023-04-29 09:05:31 +0100

    add testing for finaldata grouping

--- process.py
+++ process.py
@@ -40,28 +40,27 @@ def getStartEnd(tempData,THRESHOLD):
         if THRESHOLD > tempData.loc[i, 'diff'] >= 0 and (tempData.loc[i, 'pitch_midi'] == tempData.loc[i - 1, 'pitch_midi']):
 
             if i == 1:
-                finalData.append([startTime, tempData.loc[i,'end_time_s'], tempData.loc[i,'pitch_midi']])
-                tempData = tempdata
+                finalData.append([startTime, tempData.loc[i,'end_time_s'], tempData.loc[i,'pitch_midi'],i])
             else:
-                finalData.append([tempData.loc[i-1,'start_time_s'], tempData.loc[i, 'end_time_s'], tempData.loc[i, 'pitch_midi']])
+                finalData.append([tempData.loc[i-1,'start_time_s'], tempData.loc[i, 'end_time_s'], tempData.loc[i, 'pitch_midi'],i])
+
             startTime=tempData.loc[i,'start_time_s']
-
-
 
             #delete both old rows after appending in finaldata
 
+            print("new start time is")
+            print(startTime)
+        else:
+            #finalData.append([tempData.loc[i,'start_time_s'], tempData.loc[i, 'end_time_s'], tempData.loc[i, 'pitch_midi'],420])
+            print("loopfixer")
 
-
+    #finalData.append([tempData.loc[i-1, 'start_time_s'], tempData.loc[i, 'end_time_s'], tempData.loc[i,'pitch_midi']])
+    #finalData=finalData.reset_index()
     return(pd.DataFrame(finalData))
 
 
 finalData=pd.DataFrame()
 
-print("finalData:")
-print(finalData)
-
-print("tempData:")
-print(tempData)
 
 #call function and place results back into itself
 finalData=pd.concat([finalData,getStartEnd(data,0.02)])
@@ -70,3 +69,58 @@ print(finalData)
 
 
 #replace split notes from from original
+"""
+finalData.between(0,1).any()
+
+patients_data_list = []
+
+for patient_id, patient_df in gb_patient:
+   patient_df = patient_df.sort_values(by=['Date', 'Time'])
+   patient_data = {
+       "patient_id": patient_id,
+       "start_time": patient_df.Date.values[0] + patient_df.Time.values[0],
+       "end_time": patient_df.Date.values[-1] + patient_df.Time.values[-1]
+   }
+
+   patients_data_list.append(patient_data)
+
+new_df = pd.DataFrame(patients_data_list)
+
+finalData = finalData.sort_values()
+for i in finalData.index:
+        other_start = df.loc[i, 'start_time_s']
+        other_end = df.loc[i, 'end_time_s']
+        if (start > other_start) & (start < other_end):
+            overlaps += 1
+"""
+finalData.columns = ['start_time_s', 'end_time_s', 'pitch_midi', 'note_group']
+finalData = finalData.sort_values(['start_time_s'],ascending=[False])
+
+print(finalData)
+"""
+for f in range(1,len(finalData)):
+    start = finalData.loc[f, 'start_time_s']
+    end = finalData.loc[f, 'end_time_s']
+    interval = pd.Interval(left=start, right=end)
+    print(1 in interval)
+
+    """
+"""
+def groupNotes(tempData):
+    #tempData = tempData.reset_index()
+    print(tempData)
+    for f in range(0,len(tempData)):
+        #start = tempData.loc[[f+1], ['start_time_s']]
+        print(start)
+        #end = tempData.loc[f+1, 'end_time_s']
+
+        #interval = pd.Interval(left=start, right=end)
+        #nextstartval = finalData.loc[f, 'start_time_s']
+        #print(nextstartval)
+        #if nextstartval in interval == True:
+        #    print ("index ")
+
+    return(pd.DataFrame(tempData))
+
+finalData=groupNotes(finalData)
+"""
